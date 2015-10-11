742d436e80d86843d250be0c229dc701af851405
Mislav Marohnić <mislav.marohnic@gmail.com>
2015-10-11 04:25:01 +0200

    Handle user gems location for future 2.x versions
    Don't hardcode only 2.0 up to 2.2

--- rbenv.d/which/user-gems.bash
+++ rbenv.d/which/user-gems.bash
@@ -3,9 +3,7 @@ if [ ! -x "$RBENV_COMMAND_PATH" ]; then
   case "$RBENV_VERSION" in
   1.8* ) gemdir="ruby/1.8" ;;
   1.9* ) gemdir="ruby/1.9.1" ;;
-  2.0* ) gemdir="ruby/2.0.0" ;;
-  2.1* ) gemdir="ruby/2.1.0" ;;
-  2.2* ) gemdir="ruby/2.2.0" ;;
+  2.* ) gemdir="ruby/${RBENV_VERSION:0:3}.0" ;;
   jruby-1.* ) gemdir="jruby/1.9" ;;
   jruby-9000* ) gemdir="jruby/2.1" ;;
   rbx-2.* ) gemdir="rbx/2.1" ;;
