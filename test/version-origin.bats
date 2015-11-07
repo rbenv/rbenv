pidov/nodenv

test for version-origin hook

test/version-origin.bats
@@ -2,6 +2,13 @@
 
 load test_helper
 
+export NODENV_HOOK_PATH="${NODENV_ROOT}/nodenv.d"
+
+create_hook() {
+  mkdir -p "${NODENV_ROOT}/nodenv.d/version-origin"
+  cat > "${NODENV_ROOT}/nodenv.d/version-origin/$1" <<<"$2"
+}
+
 setup() {
   mkdir -p "$NODENV_TEST_DIR"
   cd "$NODENV_TEST_DIR"
@@ -35,4 +42,13 @@ setup() {
   touch .nodenv-version
   run nodenv-version-origin
   assert_success "${PWD}/.nodenv-version"
+}
+
+@test "reports from hook" {
+  touch .node-version
+  create_hook test.bash "NODENV_VERSION_ORIGIN=plugin"
+
+  NODENV_VERSION=1 run nodenv-version-origin
+
+  assert_success "plugin"
 }

rbenv/rbenv

test/version-origin.bats
@@ -2,6 +2,13 @@
 
 load test_helper
 
+export RBENV_HOOK_PATH="${RBENV_ROOT}/rbenv.d"
+
+create_hook() {
+  mkdir -p "${RBENV_ROOT}/rbenv.d/version-origin"
+  cat > "${RBENV_ROOT}/rbenv.d/version-origin/$1" <<<"$2"
+}
+
 setup() {
   mkdir -p "$RBENV_TEST_DIR"
   cd "$RBENV_TEST_DIR"
@@ -35,4 +42,13 @@ setup() {
   touch .rbenv-version
   run rbenv-version-origin
   assert_success "${PWD}/.rbenv-version"
+}
+
+@test "reports from hook" {
+  touch .ruby-version
+  create_hook test.bash "RBENV_VERSION_ORIGIN=plugin"
+
+  RBENV_VERSION=1 run rbenv-version-origin
+
+  assert_success "plugin"
 }