pidov/nodenv

reuse create_hook helper for hooks tests

test/hooks.bats
@@ -2,11 +2,6 @@
 
 load test_helper
 
-create_hook() {
-  mkdir -p "$1/$2"
-  touch "$1/$2/$3"
-}
-
 @test "prints usage help given no argument" {
   run nodenv-hooks
   assert_failure "Usage: nodenv hooks <command>"
@@ -15,11 +10,13 @@ create_hook() {
 @test "prints list of hooks" {
   path1="${NODENV_TEST_DIR}/nodenv.d"
   path2="${NODENV_TEST_DIR}/etc/nodenv_hooks"
-  create_hook "$path1" exec "hello.bash"
-  create_hook "$path1" exec "ahoy.bash"
-  create_hook "$path1" exec "invalid.sh"
-  create_hook "$path1" which "boom.bash"
-  create_hook "$path2" exec "bueno.bash"
+  NODENV_HOOK_PATH="$path1"
+  create_hook exec "hello.bash"
+  create_hook exec "ahoy.bash"
+  create_hook exec "invalid.sh"
+  create_hook which "boom.bash"
+  NODENV_HOOK_PATH="$path2"
+  create_hook exec "bueno.bash"
 
   NODENV_HOOK_PATH="$path1:$path2" run nodenv-hooks exec
   assert_success
@@ -33,8 +30,10 @@ OUT
 @test "supports hook paths with spaces" {
   path1="${NODENV_TEST_DIR}/my hooks/nodenv.d"
   path2="${NODENV_TEST_DIR}/etc/nodenv hooks"
-  create_hook "$path1" exec "hello.bash"
-  create_hook "$path2" exec "ahoy.bash"
+  NODENV_HOOK_PATH="$path1"
+  create_hook exec "hello.bash"
+  NODENV_HOOK_PATH="$path2"
+  create_hook exec "ahoy.bash"
 
   NODENV_HOOK_PATH="$path1:$path2" run nodenv-hooks exec
   assert_success
@@ -45,8 +44,8 @@ OUT
 }
 
 @test "resolves relative paths" {
-  path="${NODENV_TEST_DIR}/nodenv.d"
-  create_hook "$path" exec "hello.bash"
+  NODENV_HOOK_PATH="${NODENV_TEST_DIR}/nodenv.d"
+  create_hook exec "hello.bash"
   mkdir -p "$HOME"
 
   NODENV_HOOK_PATH="${HOME}/../nodenv.d" run nodenv-hooks exec

test/test_helper.bash
@@ -130,6 +130,6 @@ path_without() {
 }
 
 create_hook() {
-  mkdir -p "${NODENV_ROOT}/nodenv.d/$1"
-  cat > "${NODENV_ROOT}/nodenv.d/$1/$2" <<<"$3"
+  mkdir -p "${NODENV_HOOK_PATH}/$1"
+  cat > "${NODENV_HOOK_PATH}/$1/$2" <<<"$3"
 }

rbenv/rbenv

test/hooks.bats
@@ -2,11 +2,6 @@
 
 load test_helper
 
-create_hook() {
-  mkdir -p "$1/$2"
-  touch "$1/$2/$3"
-}
-
 @test "prints usage help given no argument" {
   run rbenv-hooks
   assert_failure "Usage: rbenv hooks <command>"
@@ -15,11 +10,13 @@ create_hook() {
 @test "prints list of hooks" {
   path1="${RBENV_TEST_DIR}/rbenv.d"
   path2="${RBENV_TEST_DIR}/etc/rbenv_hooks"
-  create_hook "$path1" exec "hello.bash"
-  create_hook "$path1" exec "ahoy.bash"
-  create_hook "$path1" exec "invalid.sh"
-  create_hook "$path1" which "boom.bash"
-  create_hook "$path2" exec "bueno.bash"
+  RBENV_HOOK_PATH="$path1"
+  create_hook exec "hello.bash"
+  create_hook exec "ahoy.bash"
+  create_hook exec "invalid.sh"
+  create_hook which "boom.bash"
+  RBENV_HOOK_PATH="$path2"
+  create_hook exec "bueno.bash"
 
   RBENV_HOOK_PATH="$path1:$path2" run rbenv-hooks exec
   assert_success
@@ -33,8 +30,10 @@ OUT
 @test "supports hook paths with spaces" {
   path1="${RBENV_TEST_DIR}/my hooks/rbenv.d"
   path2="${RBENV_TEST_DIR}/etc/rbenv hooks"
-  create_hook "$path1" exec "hello.bash"
-  create_hook "$path2" exec "ahoy.bash"
+  RBENV_HOOK_PATH="$path1"
+  create_hook exec "hello.bash"
+  RBENV_HOOK_PATH="$path2"
+  create_hook exec "ahoy.bash"
 
   RBENV_HOOK_PATH="$path1:$path2" run rbenv-hooks exec
   assert_success
@@ -45,8 +44,8 @@ OUT
 }
 
 @test "resolves relative paths" {
-  path="${RBENV_TEST_DIR}/rbenv.d"
-  create_hook "$path" exec "hello.bash"
+  RBENV_HOOK_PATH="${RBENV_TEST_DIR}/rbenv.d"
+  create_hook exec "hello.bash"
   mkdir -p "$HOME"
 
   RBENV_HOOK_PATH="${HOME}/../rbenv.d" run rbenv-hooks exec

test/test_helper.bash
@@ -125,6 +125,6 @@ path_without() {
 }
 
 create_hook() {
-  mkdir -p "${RBENV_ROOT}/rbenv.d/$1"
-  cat > "${RBENV_ROOT}/rbenv.d/$1/$2" <<<"$3"
+  mkdir -p "${RBENV_HOOK_PATH}/$1"
+  cat > "${RBENV_HOOK_PATH}/$1/$2" <<<"$3"
 }